21e54171c3f4ac6e5d1e0175f77e447ae47effdf
matthewcassi <36492182+matthewcassi@users.noreply.github.com>
2018-06-08 13:57:12 -0400

    Update openaq_average_script.py

--- openaq_average_script.py
+++ openaq_average_script.py
@@ -1,39 +1,46 @@
-import pandas as pd
-
-# values can be 'hourly', 'daily', 'weekly', 'monthly', 'half year', 'yearly'
+# values can be 'hourly', 'daily', 'weekly', 'monthly', 'half_yearly', 'yearly'
 # preferrable that the df is a time series
-def openaq_averages(df, time, parameter):
-
-    # Filter data by the parameter
-    df_param = df[df['parameter'] == parameter]
-
+def openaq_averages(file_location, time):
+    
+    # read the file from the given location into a pandas dataframe
+    df = pd.read_json(file_location)
+    
+    # set the column names for the dataframe
+    df.columns = ['date','value']
+    
+    # convert epoch time to date time object in pandas
+    df['date'] = pd.to_datetime(df['date'], unit='ms')
+    
+    # set the index as the date to turn it into a time series object
+    df = df.set_index('date')
+    
     # Check the value of the time parameter in the function
     if (time == 'hourly'):
         # resample the data by 1 hour
-        resample = df_param.resample('1H')['value'].mean()
-
+        resample = df.resample('1H')['value'].mean()
+    
     elif (time == 'daily'):
         # resample the data by 1 day
-        resample = df_param.resample('1D')['value'].mean()
-
+        resample = df.resample('1D')['value'].mean()
+    
     elif (time == 'weekly'):
         # resample the data by 1 week
-        resample = df_param.resample('1W')['value'].mean()
-
+        resample = df.resample('1W')['value'].mean()    
+    
     elif (time == 'monthly'):
         # resample the data by 1 month
-        resample = df_param.resample('1M')['value'].mean()
-
-    elif (time == 'half year'):
+        resample = df.resample('1M')['value'].mean()
+    
+    elif (time == 'half_yearly'):
         # resample the data by 6 months
-        resample = df_param.resample('6M')['value'].mean()
-
+        resample = df.resample('6M')['value'].mean()
+    
     elif (time == 'yearly'):
         # resample the data by 1 year
-        resample = df_param.resample('1Y')['value'].mean()
-
+        resample = df.resample('1Y')['value'].mean()
+    
     # a valid value is not submitted. return a message letting the user know what the value can be
     else:
         return "Not an accepted time value. Accepted values are hourly, daily, monthly, half year, or yearly."
-
+    
     return resample
